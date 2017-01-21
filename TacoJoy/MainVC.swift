2553c08c0d9b5f201f1066ee8be0932d75e0e24b
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-20 19:34:31 -0500

    Preparing the app design and file structure.

---
+++ TacoJoy/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainVC.swift
+//  TacoJoy
+//
+//  Created by Roydon Jeffrey on 1/20/17.
+//  Copyright © 2017 Italyte. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
