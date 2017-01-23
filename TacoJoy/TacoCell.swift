602b3c9d4ebba358bcce2087a92a68f3ae45aed2
Roydon Jeffrey <brushdam14823@gmail.com>
2017-01-22 20:22:02 -0500

    Deleted the prototype cell from the collectionView to then add a TacoCell swift file and a TacoCell xib file to be customized with other views.

---
+++ TacoJoy/TacoCell.swift
@@ -0,0 +1,18 @@
+//
+//  TacoCell.swift
+//  TacoJoy
+//
+//  Created by Roydon Jeffrey on 1/22/17.
+//  Copyright © 2017 Italyte. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
